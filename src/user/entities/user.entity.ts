e90c58f9b07e286882d6c6bb61e6847439dc0500
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-03 13:01:31 +0600

    validation added in main.ts

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+} from 'typeorm';
 import { Booking } from '../../booking/entities/booking.entity';
 import { Feedback } from '../../feedback/entities/feedback.entity';
 import { Reservation } from '../../reservation/entities/reservation.entity';
@@ -11,13 +17,13 @@ export class User {
   @Column({ type: 'varchar', length: 100 })
   name: string;
 
-  @Column({ type: 'varchar', length: 100, unique: true })
-  email: string;
+  @Column({ type: 'varchar', length: 100, unique: false, nullable: true })
+  email?: string;
 
   @Column({ type: 'varchar', length: 255 })
   password: string;
 
-  @Column({ type: 'varchar', length: 20 })
+  @Column({ type: 'varchar', length: 20, unique: true })
   phone: string;
 
   @Column({ type: 'text', nullable: true })
@@ -25,6 +31,33 @@ export class User {
 
   @Column({ type: 'varchar', length: 50, nullable: true })
   nid?: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  passport?: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  nationality: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  Profession?: string;
+
+  @Column({ type: 'integer', nullable: true })
+  age?: number;
+
+  @Column({ type: 'boolean', nullable: true })
+  maritalStatus?: boolean;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  vehicleNo?: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  fatherName?: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  RegistrationDate: Date;
 
   @OneToMany(() => Booking, (booking) => booking.user)
   bookings: Booking[];
