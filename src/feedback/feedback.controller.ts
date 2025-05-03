18da2c45c45e59403be9be87dd9689642f7ca293
Xerefrux <raiyansayeed0@gmail.com>
2025-05-03 21:48:49 +0600

    feedback-raiyan

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { FeedbackService } from './feedback.service';
+import { CreateFeedbackDto } from './DTOs/create-feedback.dto';
 
 @Controller('feedback')
-export class FeedbackController {}
+export class FeedbackController {
+    constructor(private readonly feedbackService: FeedbackService) {}
+
+    @Post('submit')
+    public async submitFeedback(@Body() createFeedbackDto: CreateFeedbackDto) {
+        return await this.feedbackService.submitFeedback(createFeedbackDto);
+    }
+
+    @Get('all')
+    public async getAllFeedback() {
+        return await this.feedbackService.getAllFeedback();
+    }
+    @Get('user/:user_id')
+    public async getFeedbackByUserId(@Body('user_id') userId: number) {
+        return await this.feedbackService.getFeedbackByUserId(userId);
+    }
+    @Get('date/:date')
+    public async getFeedbackByDate(@Body('date') date: Date) {
+        return await this.feedbackService.getFeebackByDate(date);
+    }
+
+}
--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Repository } from 'typeorm';
+import { Feedback } from './entities/feedback.entity';
+import { User } from 'src/user/entities/user.entity';
+import { CreateFeedbackDto } from './DTOs/create-feedback.dto';
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+    constructor(
+        @InjectRepository(Feedback)
+        private readonly feedbackRepository: Repository<Feedback>,
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+    async submitFeedback(@Body() createFeedbackDto: CreateFeedbackDto) {
+        const user = await this.userRepository.findOneBy({ user_id: createFeedbackDto.user_id });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const feedback = this.feedbackRepository.create(createFeedbackDto);
+        feedback.user = user;
+        return await this.feedbackRepository.save(feedback);
+    }
+
+    async getAllFeedback() {
+        return await this.feedbackRepository.find({ relations: ['user'] });
+    }
+
+    async getFeedbackByUserId(userId: number) {
+        const feedback = await this.feedbackRepository.find({
+            where: { user: { user_id: userId } },
+            relations: ['user'],
+        });
+        if (!feedback) {
+            throw new Error('Feedback not found for this user');
+        }
+        return feedback;
+    }
+
+    async getFeebackByDate(date: Date)
+    {
+        const feedback = await this.feedbackRepository.find({
+            where: { date: date },
+            relations: ['user'],
+        });
+        if (!feedback) {
+            throw new Error('Feedback not found for this date');
+        }
+        return feedback;
+    }
+}
