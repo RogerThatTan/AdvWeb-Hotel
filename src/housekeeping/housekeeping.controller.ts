5424a2c28296a1c8c4e040573e2246cfebb561fb
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-05-05 21:21:05 +0600

    housekeeping module update

--- src/housekeeping/housekeeping.controller.ts
+++ src/housekeeping/housekeeping.controller.ts
@@ -1,28 +1,31 @@
 import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { HousekeepingService } from './housekeeping.service';
 import { CreateHousekeepingDto } from './DTOs/create-housekeeping.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
+@Auth(AuthType.None)
 @Controller('housekeeping')
 export class HousekeepingController {
-  constructor(private readonly housekeepingService: HousekeepingService) {}
+    constructor(private readonly housekeepingService: HousekeepingService) { }
 
-  @Post('create')
-  create(@Body() dto: CreateHousekeepingDto) {
-    return this.housekeepingService.create(dto);
-  }
+    @Post('create')
+    create(@Body() dto: CreateHousekeepingDto) {
+        return this.housekeepingService.create(dto);
+    }
 
-  @Get('all')
-  findAll() {
-    return this.housekeepingService.findAll();
-  }
+    @Get('all')
+    findAll() {
+        return this.housekeepingService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.housekeepingService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.housekeepingService.findOne(+id);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.housekeepingService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.housekeepingService.remove(+id);
+    }
 }
